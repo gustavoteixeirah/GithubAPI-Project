9fb676484d2a5f4d616fb689b65586b5dc8a9d90
Gustavo Teixeira <iwhrim@gmail.com>
2021-04-24 22:52:51 -0300

    Included Swagger documentation

---
+++ src/main/java/dev/gustavoteixeira/githubstats/api/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package dev.gustavoteixeira.githubstats.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("dev.gustavoteixeira.githubstats.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title ("GitHub API")
+                .description ("This is my implementation of the following Technical Challenge proposed by a company for a BackEnd Developers opportunity.")
+                .version("1.0.0")
+                .contact(new Contact("Gustavo Teixeira",null, "iwhrim@gmail.com"))
+                .build();
+
+        return apiInfo;
+    }
+
+
+}
